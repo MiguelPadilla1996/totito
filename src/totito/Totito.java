2ca899f0ffc1cc74703eebf5167b025670b7b480
Miguel Padilla <miguelpadilla9619@gmail.com>
2024-05-07 20:23:55 -0600

    Primer cambio

---
+++ src/totito/Totito.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package totito;
+
+/**
+ *
+ * @author saman
+ */
+public class Totito {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       funcionalidad juego = new funcionalidad();
+        juego.juega();
+    }
+    
+}
+    
---
+++ src/totito/funcionalidad.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package totito;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author saman
+ */
+public class funcionalidad {
+        private char[][] tablero;
+    private char jugadorActual;
+
+    public funcionalidad(){
+ tablero = new char[3][3];
+        jugadorActual = 'X';
+        inicializarTablero();}
+
+    private void inicializarTablero() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tablero[i][j] = '-';
+            }
+        }
+    }
+
+    public void juega() {
+        Scanner scanner = new Scanner(System.in);
+        boolean juegoActivo = true;
+
+        while (juegoActivo) {
+            mostrarTablero();
+            System.out.println("Turno del jugador " + jugadorActual);
+            System.out.print("Ingrese fila y columna (por ejemplo, 1 1 para la primera fila y la primera columna): ");
+            int fila = scanner.nextInt();
+            int columna = scanner.nextInt();
+
+            if (fila >= 1 && fila <= 3 && columna >= 1 && columna <= 3 && tablero[fila - 1][columna - 1] == '-') {
+                tablero[fila - 1][columna - 1] = jugadorActual;
+                
+                if (haGanado()) {
+                    mostrarTablero();
+                    System.out.println("¡El jugador " + jugadorActual + " ha ganado!");
+                    juegoActivo = false;
+                } else if (tableroLleno()) {
+                    mostrarTablero();
+                    System.out.println("¡Empate!");
+                    juegoActivo = false;
+                } else {
+                    cambiarTurno();
+                }
+            } else {
+                System.out.println("Movimiento inválido. Inténtalo de nuevo.");
+            }
+        }
+        scanner.close();
+    }
+
+    private void mostrarTablero() {
+        System.out.println("Tablero:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void cambiarTurno() {
+        if (jugadorActual == 'X') {
+            jugadorActual = 'O';
+        } else {
+            jugadorActual = 'X';
+        }
+    }
+
+    private boolean haGanado() {
+      
+        for (int i = 0; i < 3; i++) {
+            if (tablero[i][0] != '-' && tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2]) {
+                return true;
+            }
+        }
+       
+        for (int j = 0; j < 3; j++) {
+            if (tablero[0][j] != '-' && tablero[0][j] == tablero[1][j] && tablero[1][j] == tablero[2][j]) {
+                return true;
+            }
+        }
+        
+        if (tablero[0][0] != '-' && tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) {
+            return true;
+        }
+        if (tablero[0][2] != '-' && tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0]) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean tableroLleno() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (tablero[i][j] == '-') {
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+
+    
+
+}
